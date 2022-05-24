3ba1c207eee14e00bec370a145ba3c44692c01de
Joshua Peddle <37007232+JoshuaPeddle@users.noreply.github.com>
2022-05-24 14:14:32 -0230

    Extract contact page into component

--- react/src/App.js
+++ react/src/App.js
@@ -1,18 +1,15 @@
 import "./App.css";
 import * as React from 'react';
 
-
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { red, green } from '@mui/material/colors';
-import { TextField, Box, Button, Typography,  Zoom, Fade } from "@mui/material";
+import {  Fade } from "@mui/material";
 
 
 import TypeWriter from "./components/TypeWriter";
-
 import TerminalLogin from "./components/TerminalLogin";
 
-import SendIcon from '@mui/icons-material/Send';
 
+import Contact from "./components/Contact";
 
 const theme = createTheme({
   breakpoints: {
@@ -56,70 +53,51 @@ const strings =
 
 class App extends React.Component {
 
-
-  constructor(props){
+  constructor(props) {
     super(props)
     this.fadeIn = this.fadeIn.bind(this);
-    this.state ={shouldFadeIn: false}
+    this.state = { shouldFadeIn: false, showOverflow: 'hidden' };
   }
 
-  fadeIn(){
+  fadeIn() {
     console.log("called!")
-    this.setState({shouldFadeIn:true})
+    this.setState({ shouldFadeIn: true, showOverflow: 'visible' })
   }
+
 
   render() {
-
     return (
 
       <ThemeProvider theme={theme}>
-        
 
-        <div className="App" style={{width:'100%',height:'100vh', background: theme.palette.bg.light}}>
-        <TerminalLogin username={"CrashOverride"} done={this.fadeIn}  ></TerminalLogin>
-        <Fade in={this.state.shouldFadeIn} unmountOnExit>
-          <header style={{ background: theme.palette.bg.light }} className="App-header">
-          
-          
-            <TypeWriter color={"secondary.main"} title={title} header={header} strings={strings} />
 
-            <div> <p> </p></div>
+        <div className="App" style={{ width: '100%', height: '100vh', background: theme.palette.bg.light, overflow:this.state.showOverflow }}>
+          <TerminalLogin username={"CrashOverride"} done={this.fadeIn}  ></TerminalLogin>
+          <Fade in={this.state.shouldFadeIn} style={{ transitionDelay: this.state.shouldFadeIn ? '200ms' : '0ms' }}   {...(this.state.shouldFadeIn ? { timeout: 1500 } : {})} >
+            <header style={{ background: theme.palette.bg.light }} className="App-header">
 
-          
-          </header>
+
+              <TypeWriter color={"secondary.light"} title={title} header={header} strings={strings} />
+
+              <div> </div>
+
+
+            </header>
           </Fade>
 
-          <Fade in={this.state.shouldFadeIn} unmountOnExit>
-          <header style={{ background: theme.palette.bg.main }} className="App-header">
+          <Fade in={this.state.shouldFadeIn} >
+            <header style={{ background: theme.palette.bg.main }} className="App-header">
 
-            { /*<ProjectView  title ="notesapp.cloud"/>*/}
-
-          </header>
-          </Fade>
-          <Fade in={this.state.shouldFadeIn} unmountOnExit>
-          <header style={{ background: theme.palette.bg.light }} className="App-header">
             
 
+            </header>
+          </Fade>
+          <Fade in={this.state.shouldFadeIn} >
+            <header style={{ background: theme.palette.bg.light }} className="App-header">
 
-            <Box  sx={{
-              input: { color: 'primary.main', fontSize: 20 },
-              label: { color: 'secondary.light', fontSize: 18 },
-              textarea: { color: 'primary.main', fontSize: 20 },
-              width: '95%', maxWidth: '1080px',
-    
-            }}>
-              <Typography style ={{caretColor:'transparent'}} variant="h2" component="h2" color={'primary'}> Contact</Typography>
-              <br />
-              <TextField id="filled-basic1" label="Full Name" variant="outlined" color={'secondary'} sx={{ width: "33%", minWidth: '360px', background:"#00000030" }} />
-              <TextField id="filled-basic2" autoComplete="email" label="Email" variant="outlined" color={'secondary'} sx={{ width: "33%", minWidth: '360px' , background:"#00000030"}} />
-              <TextField id="filled-basic3" label="Phone" variant="outlined" color={'secondary'} sx={{ width: "33%", minWidth: '360px' , background:"#00000030"}} />
-              <div style={{width:"100%", minHeight:"10px"}}></div>
-              <TextField id="filled-basic4"  size="large" sx={{background:"#00000030",  textarea: { fontSize: 22,   } }} label="Message" variant="outlined" rows={15} color={'secondary'} multiline fullWidth />
-              <br />
-              <Button sx={{ borderColor: 'secondary.main'  }} variant="outlined"><SendIcon sx={{ color: 'secondary.main' }} /></Button>
-            </Box>
-  
-          </header>
+              <Contact />
+
+            </header>
           </Fade>
         </div>
 
---
+++ react/src/components/Contact.js
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import { TextField, Box, Button, Typography } from "@mui/material";
+
+
+import SendIcon from '@mui/icons-material/Send';
+
+
+
+
+
+
+export default function Contact() {
+
+    const nameRef = React.useRef(null);
+    const emailRef = React.useRef(null);
+    const phoneRef = React.useRef(null);
+    const messageRef = React.useRef(null);
+
+
+
+    const sendMessage= async (name, email, phone, message)=> {
+
+        let toSend = {name:name, email:email, phone:phone, message:message}
+
+        let response = await fetch('/message', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          },
+          body: JSON.stringify(toSend)
+        });
+
+        console.log(response);
+    }
+
+    const compileMessage = ()=>{
+        let name = getName()
+        let email = getEmail()
+        let phone = getPhone()
+        let message = getMessage()
+        console.log(name, email, phone, message)
+        sendMessage(name, email, phone, message)
+    }
+
+    const getName = (e) => {
+        // Get full name field
+        return nameRef.current.value
+    }
+    const getEmail = (e) => {
+        return emailRef.current.value
+    }
+    const getPhone = (e) => {
+        return phoneRef.current.value
+
+    }
+    const getMessage = (e) => {
+        return messageRef.current.value
+    }
+
+    return (
+
+        <Box sx={{
+            input: { color: 'primary.main', fontSize: 22 },
+            label: { color: 'secondary.light', fontSize: 18 },
+            textarea: { color: 'primary.main', fontSize: 20 },
+            width: '95%', maxWidth: '1500px',
+        }}>
+            <Typography style={{ caretColor: 'transparent' }} variant="h2" component="h2" color={'primary'}> Contact</Typography>
+            <br />
+            <TextField id="filled-basic1"  inputRef={nameRef} label="Full Name" variant="outlined" color={'secondary'} sx={{ width: "33%", minWidth: '300px', background: "#00000030" }} />
+            <TextField id="filled-basic2" inputRef={emailRef}  autoComplete="email" label="Email" variant="outlined" color={'secondary'} sx={{ width: "33%", minWidth: '300px', background: "#00000030" }} />
+            <TextField id="filled-basic3" inputRef={phoneRef}  label="Phone" variant="outlined" color={'secondary'} sx={{ width: "33%", minWidth: '300px', background: "#00000030" }} />
+            <div style={{ width: "100%", minHeight: "10px" }}></div>
+            <TextField id="filled-basic4" inputRef={messageRef}   size="large" sx={{ background: "#00000030", textarea: { fontSize: 22, } }} label="Message" variant="outlined" rows={15} color={'secondary'} multiline fullWidth />
+            <br />
+            <Button sx={{ borderColor: 'secondary.main' }} onClick={compileMessage} variant="outlined"><SendIcon sx={{ color: 'secondary.main' }} /></Button>
+        </Box>
+    )
+}
