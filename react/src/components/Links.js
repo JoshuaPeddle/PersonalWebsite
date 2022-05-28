8a51329ec1e5172b75de9f2e93eaf45c4f1e12e9
Joshua Peddle <37007232+JoshuaPeddle@users.noreply.github.com>
2022-05-28 20:15:12 -0230

    Add GitHub and LinkedIn links

--- react/src/components/Links.js
+++ react/src/components/Links.js
@@ -8,17 +8,34 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 import { blue } from '@mui/material/colors';
 
+const links_to_display = {
+    github: { link: "https://github.com/JoshuaPeddle", text: "Github ", target: 'github' },
+    linkedin: { link:"https://ca.linkedin.com/in/joshua-peddle", text:'LinkedIn', target:'linkedin' }
+}
+
+
 
 export default function Links() {
+
+
+
+    const GetLinks = () =>{
+        let html = [];
+        Object.keys(links_to_display).forEach(key=>{
+            let el = links_to_display[key]
+
+            html.push(<Link key={key} link={el.link} text={el.text} target={el.target} />)
+        })
+        return html
+    }
 
     return (
 
         <Box sx={{
             width: '100%',
-            display: 'flex', justifyContent: 'flex-start', position: 'absolute', top: '0px'
+            display: 'flex', justifyContent: 'space-between', position: 'absolute', top: '0px'
         }}>
-            <Link link="https://github.com/JoshuaPeddle" text="Github " target='github' />  <Link link="https://ca.linkedin.com/in/joshua-peddle" text='LinkedIn ' target='linkedin' />
-
+            <GetLinks/>
         </Box>
     )
 }
@@ -26,9 +43,8 @@ export default function Links() {
 function Link(props) {
 
     const buttonStyle = {
-        fontSize: 50, padding: '0px', margin: '0px'
+        fontSize: "calc(49px + 1.5vw)", padding: '0px', margin: '0px'
     }
-
 
     const link = (key) => {
         switch (key) {
@@ -41,20 +57,20 @@ function Link(props) {
         }
     }
 
-
     const handleClick = () => {
-
-        window.location.href = props.link
+        window.location.assign(props.link)
     }
 
     return (
 
         <Box onClick={handleClick} sx={{
             display: 'flex', flexDirection: 'column', alignItems: 'center',
-            width: 'auto', height: 'auto', backgroundColor: '#ffffff05', borderRadius: '10px', padding: '10px'
+            width: 'auto', height: 'auto', background: 'radial-gradient(circle,  rgba(255,255,255,0.04) 0%, rgba(128,128,200,0.05) 20%, rgba(128,128,200,0.08) 100%);', 
+            borderRadius: '20px', padding: "calc(5px + 1vw)", borderStyle:'outset', borderColor:'rgba(128,128,200,0.08)', borderWidth:'5px', margin:'1%'
         }}>
 
-            <Typography sx={{ color: "#EEF0F2bb", padding: '0px', margin: '0px' }} variant={'h5'}> {props.text} </Typography>{link(props.target)}
+            <Typography sx={{ fontSize: "calc(16px + 0.7vw)",color: "#EEF0F2bb", padding: '0px', margin: '0px' }} variant={'h5'}> {props.text} </Typography>{link(props.target)}
+
         </Box>
     )
 
