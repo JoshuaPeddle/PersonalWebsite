d09c91eb2d7bfa867d352f3dadb710d1b511055e
Joshua Peddle <37007232+JoshuaPeddle@users.noreply.github.com>
2022-05-29 19:49:25 -0230

    Serve new react index

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -7,7 +8,7 @@ router.get('/', function (req, res) {
 	if(req.user){  // If user has an active session skip login page
 		res.send('Logged In!');
 	}else{   // No active session, go to login page
-		res.redirect('login.html');
+		res.sendFile(path.join(__dirname,'..', 'build', 'index.html'));
 	}
 });
 
