a6fa5082a89c627bea9657770a0e1bc6be2d2ed1
Joshua Peddle <37007232+JoshuaPeddle@users.noreply.github.com>
2022-05-30 12:40:15 -0230

    Rework this router with react front-end in mind

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,14 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function (req, res) {
-	res.set('Cache-control', 'no-cache');
-	if(req.user){  // If user has an active session skip login page
-		res.send('Logged In!');
-	}else{   // No active session, go to login page
-		res.sendFile(path.join(__dirname,'..', 'build', 'index.html'));
+	if (process.env.NODE_ENV === 'production') {
+		res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+	} else {
+		res.send('Server misconfigured, launch with NODE_ENV=production or start react app');
 	}
+
 });
 
-
-/* GET home page. */
-router.post('/', function (req, res) {
-	res.set('Cache-control', 'no-cache');
-
-	console.log('hello');
-	res.sendStatus(200);
-});
 
 
 module.exports = router;
